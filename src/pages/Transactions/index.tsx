c16bc4bcc1c5f1f41c5685638316f763eb5d5738
Alladin-51 <igortls20@gmail.com>
2024-10-15 18:19:26 -0300

    Tabela de transições

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionTable, TrasactionContainer } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TrasactionContainer>
+                <TransactionTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento do site</td>
+                            <td >
+                                <PriceHighLight variant="income">
+                                    R$ 12.000
+                                </PriceHighLight>    
+                            </td>
+                            <td>Venda</td>
+                            <td>20/02/2021</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburger</td>
+                            <td >
+                                <PriceHighLight variant="outcome">
+                                        R$ 50.00
+                                </PriceHighLight>   
+                            </td>
+                            <td>Alimentação</td>
+                            <td>20/03/2021</td>
+                        </tr>  
+                    </tbody>
+                </TransactionTable>
+                </TrasactionContainer>
         </div>
     )
 }
