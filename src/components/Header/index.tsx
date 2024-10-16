8338fb64752f997e656cbedafe333925bc013812
Alladin-51 <igortls20@gmail.com>
2024-10-16 12:32:21 -0300

    Criando modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-
+import * as Dialog from "@radix-ui/react-dialog";
 import logoImg from '../../assets/Logo-ignite.svg';
 
 export function Header() {
@@ -8,9 +8,23 @@ export function Header() {
             <HeaderContent>
 
                 <img src={logoImg} alt="" />
-                <NewTransactionButton>
-                     Nova Transação
-                </NewTransactionButton>
+
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>  
+                        <NewTransactionButton>Nova Transação</NewTransactionButton>
+                    </Dialog.Trigger>
+                    <Dialog.Portal>
+                        <Dialog.Overlay/>
+                        <Dialog.Content>
+                            <div>
+                            <Dialog.Title>Nova Transação</Dialog.Title>
+                            <Dialog.Close/>
+                            </div>
+                        </Dialog.Content>
+                    </Dialog.Portal>
+
+                </Dialog.Root>
+
             </HeaderContent>
         </HeaderContainer>
     )
