532454b31e77d92a7bdd72be4f9d8a0f4a90a410
Alladin-51 <igortls20@gmail.com>
2024-10-17 15:57:13 -0300

    Adicionando calculo do summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -5,7 +5,21 @@ import { TransactionContext } from "../../contexts/TransactionsContexts";
 
 export function Summary() {
     const {transactions} = useContext(TransactionContext);
-    console.log(transactions);
+    
+    const summary = transactions.reduce(
+        (acc, transaction) => {
+
+            if(transaction.type === 'income'){
+                acc.income += transaction.price;
+                acc.total += transaction.price;
+            } else {
+                acc.outcome += transaction.price;
+                acc.total -= transaction.price;
+            }
+            return acc;
+        },
+        {income: 0, outcome: 0, total: 0});
+    //Esse método reduce serve para mudar um tipo de dado para outro, nesse caso, ele está mudando um array de objetos para um objeto com 3 propriedades
 
     return (
         <SummaryContainer>
@@ -14,7 +28,7 @@ export function Summary() {
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color="#00b37e" />
                 </header>
-                <strong>R$ 1000,00</strong>
+                <strong>{summary.income}</strong>
 
             </SummaryCard>
             <SummaryCard>
@@ -22,7 +36,7 @@ export function Summary() {
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
-                <strong>R$ 500,00</strong>
+                <strong>{summary.outcome}</strong>
             </SummaryCard>
 
             <SummaryCard variant="green">
@@ -30,7 +44,7 @@ export function Summary() {
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#fff" />
                 </header>
-                <strong>R$ 500,00</strong>
+                <strong>{summary.total}</strong>
             </SummaryCard>
 
 
