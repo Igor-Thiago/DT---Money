00218b71df122ff3e430931d5da9ddbda1164227
Alladin-51 <igortls20@gmail.com>
2024-10-18 14:17:23 -0300

    Formatando preços e datas

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,6 +4,7 @@ import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionTable, TrasactionContainer } from "./styles";
 import { TransactionContext } from "../../contexts/TransactionsContexts";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
 
@@ -23,11 +24,12 @@ export function Transactions() {
                                 <td width="50%">{transaction.description}</td>
                                 <td >
                                     <PriceHighLight variant={transaction.type}>
-                                        {transaction.price}
+                                        {transaction.type === 'outcome' && '- '} 
+                                        {priceFormatter.format(transaction.price)}
                                     </PriceHighLight>    
                                 </td>
                                 <td>{transaction.category}</td>
-                                <td>{transaction.createdAt}</td>
+                                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                             </tr>
                             )
                         })}
